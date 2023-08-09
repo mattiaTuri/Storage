b8a35023be2994cd56eb9e1f8182c6068692d969
mattiaTuri <turina.mattia@gmail.com>
2023-08-09 11:50:26 +0200

    fixed bg color on card summary

--- src/components/feature/dashboard/cards/summary/StatisticsNumberBox.tsx
+++ src/components/feature/dashboard/cards/summary/StatisticsNumberBox.tsx
@@ -1,31 +1,41 @@
 import Typography from "@mui/material/Typography";
 import AnimatedNumber from "../../../../shared/AnimatedNumber";
 import Loader from "../../../../shared/Loader";
+import Box from "@mui/material/Box";
 
-interface StatisticsNumberBoxProps{
-    title:string;
-    data:any;
-    loading:boolean;
-    primaryColor:string;
-    secondaryColor:string;
+interface StatisticsNumberBoxProps {
+  title: string;
+  data: number;
+  loading: boolean;
+  primaryColor: string;
+  secondaryColor: string;
 }
 
-function StatisticsNumberBox({title, data, loading, primaryColor, secondaryColor}: StatisticsNumberBoxProps){
-    return (
-        <div className={`flex justify-between items-center p-3 bg-[${primaryColor}] rounded-md w-full`}>
-            <Typography component="p">{title}</Typography>
-
-            <div
-                className={`bg-[${secondaryColor}] max-w-max px-4 text-center rounded-md`}
-            >
-                {loading ? (
-                <AnimatedNumber listLength={data.length}/>
-                ) : (
-                <Loader size={10} color={secondaryColor}/>
-                )}
-            </div>
-        </div>
-    )
+function StatisticsNumberBox({
+  title,
+  data,
+  loading,
+  primaryColor,
+  secondaryColor,
+}: StatisticsNumberBoxProps) {
+  return (
+    <Box
+      className="flex justify-between items-center p-3 rounded-md w-full"
+      sx={{ backgroundColor: primaryColor }}
+    >
+      <Typography component="p">{title}</Typography>
+      <Box
+        className="max-w-max px-4 text-center rounded-md"
+        sx={{ backgroundColor: secondaryColor }}
+      >
+        {loading ? (
+          <AnimatedNumber listLength={data} />
+        ) : (
+          <Loader size={10} color={secondaryColor} />
+        )}
+      </Box>
+    </Box>
+  );
 }
 
-export default StatisticsNumberBox
+export default StatisticsNumberBox;
--- src/components/feature/dashboard/cards/summary/SummaryCard.tsx
+++ src/components/feature/dashboard/cards/summary/SummaryCard.tsx
@@ -1,33 +1,50 @@
-import Card from "@mui/material/Card"
-import CardContent from "@mui/material/CardContent"
-import Typography from "@mui/material/Typography"
-import StatisticsNumberBox from "./StatisticsNumberBox"
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import StatisticsNumberBox from "./StatisticsNumberBox";
 import { useSelector } from "react-redux";
 import { booksListSelector } from "../../../../../store/booksList/selector";
 import { resourcesListSelector } from "../../../../../store/resourcesList/selector";
 
-function SummaryCard(){
+function SummaryCard() {
+  const bookList = useSelector(booksListSelector);
+  const resourcesList = useSelector(resourcesListSelector);
 
-    const bookList = useSelector(booksListSelector);
-    const resourcesList = useSelector(resourcesListSelector);
-
-    return (
-        <Card className="bg-white dark:bg-[#262626] dark:border-[#434343] border">
-            <CardContent className="text-white flex flex-col justify-center h-full gap-2">
-                <Typography
-                gutterBottom
-                variant="h5"
-                component="h2"
-                className="text-[#474862] dark:text-white"
-                >
-                Summary
-                </Typography>
-                <StatisticsNumberBox title="Books" data={bookList.books} loading={bookList.loading} primaryColor="#efa135" secondaryColor="#ffbf00"/>
-                <StatisticsNumberBox title="Resources" data={resourcesList.resources} loading={resourcesList.loading} primaryColor="#0066ff" secondaryColor="#17c0fd"/>
-                <StatisticsNumberBox title="Tasks" data={bookList.books} loading={bookList.loading} primaryColor="#522aa7" secondaryColor="#6d39de"/> 
-            </CardContent>
-        </Card>
-    )
+  return (
+    <Card className="bg-white dark:bg-[#262626] dark:border-[#434343] border">
+      <CardContent className="text-white flex flex-col justify-center h-full gap-2">
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="h2"
+          className="text-[#474862] dark:text-white"
+        >
+          Summary
+        </Typography>
+        <StatisticsNumberBox
+          title="Books"
+          data={bookList.books.length}
+          loading={bookList.loading}
+          primaryColor="#efa135"
+          secondaryColor="#ffbf00"
+        />
+        <StatisticsNumberBox
+          title="Resources"
+          data={resourcesList.resources.length}
+          loading={resourcesList.loading}
+          primaryColor="#0066ff"
+          secondaryColor="#17c0fd"
+        />
+        <StatisticsNumberBox
+          title="Tasks"
+          data={0}
+          loading={bookList.loading}
+          primaryColor="#522aa7"
+          secondaryColor="#6d39de"
+        />
+      </CardContent>
+    </Card>
+  );
 }
 
-export default SummaryCard
+export default SummaryCard;
