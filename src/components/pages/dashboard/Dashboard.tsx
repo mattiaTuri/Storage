3b554795c7d3478c0a0cc5d03db6af8e652aedff
mattiaTuri <turina.mattia@gmail.com>
2023-08-12 13:46:03 +0200

    fixed charts position on resize page

--- src/components/pages/dashboard/Dashboard.tsx
+++ src/components/pages/dashboard/Dashboard.tsx
@@ -36,7 +36,7 @@ function Dashboard() {
               <SummaryCard />
               <TasksCard />
             </div>
-            <div className="flex flex-col gap-10 lg:grid grid-cols-2 h-[60%]">
+            <div className="flex flex-col gap-10 md:grid grid-cols-2 h-[60%]">
               <Card className="bg-white dark:bg-[#262626] dark:border-[#434343] border grid grid-rows-[100px_auto] relative h-[300px] lg:h-full">
                 {bookList.loading ? (
                   <BooksChart />
