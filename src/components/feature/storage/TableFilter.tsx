3da0fd3b4d974ec96a4d53144e0792c2fcd47b82
mattiaTuri <turina.mattia@gmail.com>
2023-11-06 13:03:57 +0100

    on the filters only the added genres will be shown

--- src/components/feature/storage/TableFilter.tsx
+++ src/components/feature/storage/TableFilter.tsx
@@ -1,27 +1,55 @@
-import { SelectChangeEvent } from "@mui/material"
-import Box from "@mui/material/Box"
+import { SelectChangeEvent } from "@mui/material";
+import Box from "@mui/material/Box";
 import { useTranslation } from "react-i18next";
 import { TableFilterProps } from "../../../models/ComponentsModels";
 import SelectBox from "../../shared/SelectBox";
-import { genresList } from "../../../date/genresList";
+import { useEffect, useState } from "react";
+import { getChartValue } from "../dashboard/charts/chartsFunctions";
+import { useSelector } from "react-redux";
+import { booksSelector } from "../../../store/books/selector";
+import { BooksProps } from "../../../models/Book";
 
-function TableFilter({genreName, setGenreName} : TableFilterProps){
-    const { t } = useTranslation();
+function TableFilter({ genreName, setGenreName }: TableFilterProps) {
+  const { t } = useTranslation();
+  const [genresList, setGenresList] = useState<any[]>([]);
+  const books = useSelector(booksSelector);
 
-      const multipleSelectionGenre = (event: SelectChangeEvent<typeof genreName>) => {
-        const {
-          target: { value },
-        } = event;
-        setGenreName(
-          typeof value === 'string' ? value.split(',') : value,
-        );
-      };
+  const multipleSelectionGenre = (
+    event: SelectChangeEvent<typeof genreName>
+  ) => {
+    const {
+      target: { value },
+    } = event;
+    setGenreName(typeof value === "string" ? value.split(",") : value);
+  };
 
-    return (
-        <Box className="flex flex-col gap-10 h-full w-full mt-10">
-           <SelectBox id="genre-filter" label={`${t("genre")}`} selectLabel={`${t("genre")} *`} name="genre" value={genreName} onChange={(e) => multipleSelectionGenre(e)} fixedLabel={false} labelError="" errorVisibility={false} objList={genresList} multiple={true}/>
-        </Box>
-    )
+  useEffect(() => {
+    const genres = books.booksList.map((elem: BooksProps) => elem.genre);
+    const newGenresList = getChartValue(genres);
+    const genresTranslated: any[] = newGenresList.map((genre) => {
+      const name = genre.name;
+      return { key: name, translation: t(`genres.${name}`) };
+    });
+    setGenresList(genresTranslated);
+  }, []);
+
+  return (
+    <Box className="flex flex-col gap-10 h-full w-full mt-10">
+      <SelectBox
+        id="genre-filter"
+        label={`${t("genre")}`}
+        selectLabel={`${t("genre")} *`}
+        name="genre"
+        value={genreName}
+        onChange={(e) => multipleSelectionGenre(e)}
+        fixedLabel={false}
+        labelError=""
+        errorVisibility={false}
+        objList={genresList}
+        multiple={true}
+      />
+    </Box>
+  );
 }
 
-export default TableFilter
+export default TableFilter;
--- src/components/feature/storage/books/BookFields.tsx
+++ src/components/feature/storage/books/BookFields.tsx
@@ -1,6 +1,6 @@
 import Box from "@mui/material/Box";
 import { useTranslation } from "react-i18next";
-import {Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import TextInput from "../TextInput";
 import { useSelector } from "react-redux";
 import { errorsSelector } from "../../../../store/errors/selector";
@@ -11,16 +11,22 @@ import SelectBox from "../../../shared/SelectBox";
 import { genresList } from "../../../../date/genresList";
 
 interface BooksFieldProps {
-  bookValues: BooksProps
+  bookValues: BooksProps;
   onValChanges: (e: ChangeEvent<HTMLInputElement>) => void;
-  onValSelected: (e:any) => void;
-  onValChangesYear: (e:any) => void;
+  onValSelected: (e: any) => void;
+  onValChangesYear: (e: any) => void;
   onValRating: (e: any) => void;
 }
 
-function BooksField({ bookValues, onValChanges, onValSelected, onValChangesYear, onValRating }: BooksFieldProps) {
-  const { t } = useTranslation()
-  const errors = useSelector(errorsSelector)
+function BooksField({
+  bookValues,
+  onValChanges,
+  onValSelected,
+  onValChangesYear,
+  onValRating,
+}: BooksFieldProps) {
+  const { t } = useTranslation();
+  const errors = useSelector(errorsSelector);
 
   return (
     <Box
@@ -29,19 +35,61 @@ function BooksField({ bookValues, onValChanges, onValSelected, onValChangesYear,
       component="form"
     >
       <Box className="flex flex-col gap-10">
-        <TextInput id="title" label={`${t("title")} *`} name="title" onChange={(e) => onValChanges(e)} autofocus={true} labelError={errors.titleError.label} errorVisibility={errors.titleError.errorVisibility}/>
-        <TextInput id="author" label={t("author")} name="author" onChange={(e) => onValChanges(e)} autofocus={false} labelError={""} errorVisibility={false}/>
-        <SelectBox id="genre" label={`${t("genre")} *`} selectLabel={`${t("genre")} *`} name="genre" value={bookValues.genre} onChange={(e) => onValSelected(e)} fixedLabel={false} labelError={errors.genreError.label} errorVisibility={errors.genreError.errorVisibility} objList={genresList} multiple={false}/>
+        <TextInput
+          id="title"
+          label={`${t("title")} *`}
+          name="title"
+          onChange={(e) => onValChanges(e)}
+          autofocus={true}
+          labelError={errors.titleError.label}
+          errorVisibility={errors.titleError.errorVisibility}
+        />
+        <TextInput
+          id="author"
+          label={t("author")}
+          name="author"
+          onChange={(e) => onValChanges(e)}
+          autofocus={false}
+          labelError={""}
+          errorVisibility={false}
+        />
+        <SelectBox
+          id="genre"
+          label={`${t("genre")} *`}
+          selectLabel={`${t("genre")} *`}
+          name="genre"
+          value={bookValues.genre}
+          onChange={(e) => onValSelected(e)}
+          fixedLabel={false}
+          labelError={errors.genreError.label}
+          errorVisibility={errors.genreError.errorVisibility}
+          objList={genresList}
+          multiple={false}
+        />
       </Box>
       <Box className="flex items-center gap-10">
         <Box className="flex gap-2 items-center">
-          <Typography className="w-max" component="legend">{`${t("rating")} *`}</Typography>
-          <RatingStars functionChange={(e) => onValRating(e)}/>
+          <Typography className="w-max" component="legend">{`${t(
+            "rating"
+          )} *`}</Typography>
+          <RatingStars functionChange={(e) => onValRating(e)} />
         </Box>
-        <TextInput id="reading_year" label={`${t("reading_year")} *`} name="reading_year" type="tel" value={bookValues.reading_year} onChange={(e) => onValChangesYear(e)} autofocus={false} labelError={errors.readingYearError.label} errorVisibility={errors.readingYearError.errorVisibility}/>
+        <TextInput
+          id="reading_year"
+          label={`${t("reading_year")} *`}
+          name="reading_year"
+          type="tel"
+          value={bookValues.reading_year}
+          onChange={(e) => onValChangesYear(e)}
+          autofocus={false}
+          labelError={errors.readingYearError.label}
+          errorVisibility={errors.readingYearError.errorVisibility}
+        />
       </Box>
       <Box>
-        <Typography component="span" variant="caption">* = {t("required_fields")}</Typography>
+        <Typography component="span" variant="caption">
+          * = {t("required_fields")}
+        </Typography>
       </Box>
     </Box>
   );
